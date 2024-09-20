a4f7593e8a0b71f873b99760d12a9720f5d1ce2f
Teja Kumar <teja.intern@srifincredit.com>
2024-09-20 22:39:43 +0530

    boiler plate spring boot code

---
+++ src/main/java/com/srifin/SecureAuth/SecureAuthApplication.java
@@ -0,0 +1,13 @@
+package com.srifin.SecureAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/srifin/SecureAuth/SecureAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srifin.SecureAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
